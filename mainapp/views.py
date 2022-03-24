2c7f97542a210d05801c3e88edcae4ec9e139c31
Vitaliy0973 <arkodianec1@gmail.com>
2022-03-24 22:19:10 +0300

    ДЗ второго урока

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+
+    content = {
+        'title': 'geekshop',
+    }
+
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+
+    categories = [
+        {'name': 'Новинки'},
+        {'name': 'Одежда'},
+        {'name': 'Обувь'},
+        {'name': 'Аксессуары'},
+        {'name': 'Подарки'},
+    ]
+
+    with open('mainapp/templates/mainapp/fixtures/product_cards.json', 'r', encoding='utf-8') as file:
+        product_cards = json.load(file)
+
+    content = {
+        'title': 'geekshop - каталог',
+        'categories': categories,
+        'product_cards': product_cards,
+    }
+
+    return render(request, 'mainapp/products.html', content)
