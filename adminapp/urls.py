9046ac86f665e012cc1953eb84d7ccb5fa5eed8e
Vitaliy0973 <arkodianec1@gmail.com>
2022-04-25 11:08:28 +0300

    homework for the lesson_8

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,25 +1,55 @@
 from django.urls import path
-from adminapp.views import admin_categories, admin_category_create, admin_category_delete, admin_category_update, admin_product_create, admin_product_delete, admin_product_update, admin_products, index, admin_users, admin_user_create, admin_user_update, admin_user_delete
+from adminapp.views import CategoriesListView, CategoryCreateView, CategoryDeleteView, CategoryUpdateView, IndexTemplateView, ProductCreateView, ProductDeleteView, ProductListView, ProductUpdateView, UserDeleteView, UserListView, UserCreateView, UserUpdateView
 
 
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
-    path('user-create/', admin_user_create, name='admin_user_create'),
-    path('user-update/<int:id>/', admin_user_update, name='admin_user_update'),
-    path('user-delete/<int:id>/', admin_user_delete, name='admin_user_delete'),
-    path('categories/', admin_categories, name='admin_categories'),
-    path('category-create/', admin_category_create, name='admin_category_create'),
-    path('category-update/<int:id>/', admin_category_update,
+    #     path('', index, name='index'),
+    path('', IndexTemplateView.as_view(), name='index'),
+
+    #     path('users/', admin_users, name='admin_users'),
+    path('users/', UserListView.as_view(), name='admin_users'),
+
+    #     path('user-create/', admin_user_create, name='admin_user_create'),
+    path('user-create/', UserCreateView.as_view(), name='admin_user_create'),
+
+    #     path('user-update/<int:id>/', admin_user_update, name='admin_user_update'),
+    path('user-update/<int:pk>/', UserUpdateView.as_view(),
+         name='admin_user_update'),
+
+    #     path('user-delete/<int:id>/', admin_user_delete, name='admin_user_delete'),
+    path('user-delete/<int:pk>/', UserDeleteView.as_view(),
+         name='admin_user_delete'),
+
+    #     path('categories/', admin_categories, name='admin_categories'),
+    path('categories/', CategoriesListView.as_view(), name='admin_categories'),
+
+    #     path('category-create/', admin_category_create, name='admin_category_create'),
+    path('category-create/', CategoryCreateView.as_view(),
+         name='admin_category_create'),
+
+    #     path('category-update/<int:id>/', admin_category_update, name='admin_category_update'),
+    path('category-update/<int:pk>/', CategoryUpdateView.as_view(),
          name='admin_category_update'),
-    path('category-delete/<int:id>/', admin_category_delete,
+
+    #     path('category-delete/<int:id>/', admin_category_delete,name='admin_category_delete'),
+    path('category-delete/<int:pk>/', CategoryDeleteView.as_view(),
          name='admin_category_delete'),
-    path('products/', admin_products, name='admin_products'),
-    path('product-create/', admin_product_create, name='admin_product_create'),
-    path('product-update/<int:id>/', admin_product_update,
+
+    #     path('products/', admin_products, name='admin_products'),
+    path('products/', ProductListView.as_view(), name='admin_products'),
+
+    #     path('product-create/', admin_product_create, name='admin_product_create'),
+    path('product-create/', ProductCreateView.as_view(),
+         name='admin_product_create'),
+
+    #     path('product-update/<int:id>/', admin_product_update,name='admin_product_update'),
+    path('product-update/<int:pk>/', ProductUpdateView.as_view(),
          name='admin_product_update'),
-    path('product-delete/<int:id>/', admin_product_delete,
+
+    #     path('product-delete/<int:id>/', admin_product_delete,name='admin_product_delete'),
+    path('product-delete/<int:pk>/', ProductDeleteView.as_view(),
          name='admin_product_delete'),
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,8 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import DetailView, ListView
+from adminapp.mixin import BaseClassContextMixin
 from mainapp.models import ProductCategories, Product
 import json
 import os
@@ -8,9 +12,9 @@ import os
 MODULE_DIR = os.path.dirname(__file__)
 
 
-def read_file(name):
-    file_path = os.path.join(MODULE_DIR, name)
-    return json.load(open(file_path, encoding='utf-8'))
+# def read_file(name):
+#     file_path = os.path.join(MODULE_DIR, name)
+#     return json.load(open(file_path, encoding='utf-8'))
 
 
 def index(request):
@@ -22,18 +26,38 @@ def index(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
 
-    categories = ProductCategories.objects.all()
-    products = Product.objects.all()
+    if id_category:
+        products = Product.objects.filter(id=id_category)
+    else:
+        products = Product.objects.all()
+
+    pagination = Paginator(products, per_page=2)
+
+    try:
+        product_pagination = pagination.page(page)
+    except PageNotAnInteger:
+        # Если мы не понимаем на какой странице находимся, нас возвращает на первую
+        product_pagination = pagination.page(1)
+    except EmptyPage:
+        # Если мы не знаем какое кол-во страниц
+        product_pagination = pagination.page(pagination.num_pages)
 
     # categories = read_file('fixtures/categories.json')
     # products = read_file('fixtures/products.json')
 
     content = {
         'title': 'geekshop - каталог',
-        'categories': categories,
-        'products': products,
+        'categories': ProductCategories.objects.all(),
+        'products': product_pagination,
     }
 
     return render(request, 'mainapp/products.html', content)
+
+
+class ProductDetail(DetailView):
+    # Специальный класс DetailView помогает нам детализировать данные
+
+    model = Product
+    template_name = 'mainapp/detail.html'
