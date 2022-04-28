fdbce9b70eed011e9957f393dd2bc4f4b7c5cc7c
Vitaliy0973 <arkodianec1@gmail.com>
2022-04-28 16:28:41 +0300

    homework for the lesson_2_1

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from authapp.views import login, register, logout, profile
+from authapp.views import LoginGBView, ProfileUpdateView, RegisterView, Logout
 
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, name='logout'),
-    path('profile/', profile, name='profile')
+    # path('login/', login, name='login'),
+    path('login/', LoginGBView.as_view(), name='login'),
+
+    # path('register/', register, name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
+
+    # path('logout/', logout, name='logout'),
+    path('logout/', Logout.as_view(), name='logout'),
+
+    # path('profile/', profile, name='profile'),
+    path('profile/', ProfileUpdateView.as_view(), name='profile'),
+
+    path('verify/<str:email>/<str:activate_key>/',
+         RegisterView.verify, name='verify')
+
 ]
